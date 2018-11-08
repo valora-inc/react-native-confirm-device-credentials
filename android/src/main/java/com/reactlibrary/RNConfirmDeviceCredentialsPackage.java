65d2d466d7eeca8d42e7b6e9a2d1ef76e660eb1b
Ashish Bhatia <ashishb@celo.org>
2018-11-08 15:38:42 -0800

    Add missing imports

--- android/src/main/java/com/reactlibrary/RNConfirmDeviceCredentialsPackage.java
+++ android/src/main/java/com/reactlibrary/RNConfirmDeviceCredentialsPackage.java
@@ -10,6 +10,10 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
+import android.app.KeyguardManager;
+import android.content.Context;
+
 public class RNConfirmDeviceCredentialsPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
