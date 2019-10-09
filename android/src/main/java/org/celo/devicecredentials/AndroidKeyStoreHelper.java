436afa981889b51178f959f0c8cd137cc46386f1
Connor McEwen <connor.mcewen@gmail.com>
2019-10-09 16:55:22 -0700

    Androidx fix

--- android/src/main/java/org/celo/devicecredentials/AndroidKeyStoreHelper.java
+++ android/src/main/java/org/celo/devicecredentials/AndroidKeyStoreHelper.java
@@ -10,7 +10,7 @@ import android.content.Intent;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 import android.security.keystore.UserNotAuthenticatedException;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import javax.crypto.*;
